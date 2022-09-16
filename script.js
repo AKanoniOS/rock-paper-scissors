e6452456a94ab7115744049c75e37484a52343ff
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-09-16 08:48:58 -0800

    add playRound function

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ let choices = ["rock","paper","scissors"];
 
 function getComputerChoice(arr) {
     let choice = arr[Math.floor(Math.random()*arr.length)];
-    console.log(choice);
+    return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection == computerSelection) {
+        console.log(`You both played ${playerSelection}`);
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            console.log("you lose, paper beats rock");
+        } else if (computerSelection == "scissors") {
+            console.log("you win, rock beats scissors");
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            console.log("you lose, scissors beats paper");
+        } else if (computerSelection == "rock") {
+            console.log("you win, paper beats rock");
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            console.log("you lose, rock beats scissors");
+        } else if (computerSelection == "paper") {
+            console.log("you win, scissors beats paper");
+        }
+    }
   }
    
-  const playerSelection = "rock";
+  const playerSelection = "scissors";
   const computerSelection = getComputerChoice(choices);
-//   console.log(playRound(playerSelection, computerSelection));
+  console.log(playRound(playerSelection, computerSelection));
