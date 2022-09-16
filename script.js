c841e7e701240081265de5641ea438f298bf7731
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-09-16 12:12:05 -0800

    add score display and add status message

--- script.js
+++ script.js
@@ -5,30 +5,51 @@ function getComputerChoice(arr) {
     return choice;
 }
 
+let playerSelection = "rock";
+let computerSelection = getComputerChoice(choices);
+let playerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
+    playerSelection = "rock";
+    computerSelection = getComputerChoice(choices);
+
     if (playerSelection == computerSelection) {
-        console.log(`You both played ${playerSelection}`);
+        display.textContent=(`You both played ${playerSelection}`);
     } else if (playerSelection == "rock") {
         if (computerSelection == "paper") {
-            console.log("you lose, paper beats rock");
+            display.textContent=("you lose, paper beats rock");
         } else if (computerSelection == "scissors") {
-            console.log("you win, rock beats scissors");
+            display.textContent=("you win, rock beats scissors");
+            playerScore += 1;
+            score.textContent = `Player Score: ${playerScore}`;
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "scissors") {
-            console.log("you lose, scissors beats paper");
+            display.textContent=("you lose, scissors beats paper");
         } else if (computerSelection == "rock") {
-            console.log("you win, paper beats rock");
+            display.textContent=("you win, paper beats rock");
+            playerScore += 1;
+            score.textContent = `Player Score: ${playerScore}`;
         }
     } else if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            console.log("you lose, rock beats scissors");
+            display.textContent=("you lose, rock beats scissors");
         } else if (computerSelection == "paper") {
-            console.log("you win, scissors beats paper");
+            display.textContent=("you win, scissors beats paper");
+            playerScore += 1;
+            score.textContent = `Player Score: ${playerScore}`;
         }
     }
   }
+
+
+
+
+let score = document.querySelector(".score");
+score.textContent = `Player Score: ${playerScore}`;
+
+let display = document.querySelector(".message");
+display.textContent = `Nothing has been played yet`;
    
-  const playerSelection = "scissors";
-  const computerSelection = getComputerChoice(choices);
-  console.log(playRound(playerSelection, computerSelection));
+
+// console.log(playRound(playerSelection, computerSelection));
